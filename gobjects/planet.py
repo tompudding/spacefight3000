8a0c102b763999d3b1f571167e456b105666cca5
Tom Gooding <tom.pudding@gmail.com>
2014-08-24 15:58:31 +0100

    Sort of align the portals

--- gobjects/planet.py
+++ gobjects/planet.py
@@ -3,15 +3,23 @@ import globals
 from globals.types import Point
 import cmath
 import drawing
+import math
 
 class PortalEnd(gobject.CircleGobject):
     texture_name = 'gate'
-    def __init__(self,bl,tr):
+    def __init__(self,bl,tr,angle):
         self.tc = [globals.atlas.TextureSpriteCoords(self.texture_name + '%d.png' % i) for i in xrange(6)]
         super(PortalEnd,self).__init__(bl,tr,self.tc)
+        self.body.angle = angle
+        self.PhysUpdate([])
 
     def InitPolygons(self,tcs):
         self.quad = drawing.Quad(globals.quad_buffer, tc = tcs[0])
+        #self.body.angle = 2
+
+    def CreateShape(self,midpoint,pos = None):
+        return super(PortalEnd,self).CreateShape(midpoint,pos)
+
 
 class Portal(object):
     def __init__(self,source_planet,source_angle,target_planet,target_angle):
@@ -19,7 +27,7 @@ class Portal(object):
         for planet,angle in (source_planet,source_angle),(target_planet,target_angle):
             bl = planet.GetSurfacePoint(angle)
             tr = bl + Point(50,50)
-            self.ends.append(PortalEnd(bl,tr))
+            self.ends.append(PortalEnd(bl,tr,angle+3*math.pi/2))
 
 
 class Planet(gobject.CircleGobject):
