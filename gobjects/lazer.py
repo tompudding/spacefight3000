53870119ae8daddb46d82c2d3788c04bd23f32c5
Tom Gooding <tom.pudding@gmail.com>
2014-08-25 17:36:12 +0100

    Added some more sounds

--- gobjects/lazer.py
+++ gobjects/lazer.py
@@ -1,16 +1,18 @@
 import weapon
 from globals.types import Point
+import sounds
+import globals
 
 class Lazer(weapon.Weapon):
     #lazers should probably be a limited shot weapon, since gravity wont affect them in the same way as other weapons. Maybe do a touch more damage
-    #than the default bazooka as well. 
-    
+    #than the default bazooka as well.
     def __init__(self):
-        self.projectile_texture_name = "laser.png" 
+        self.fire_sound = globals.sounds.laser_fire
+        self.projectile_texture_name = "laser.png"
         super(Lazer, self).__init__(75, self.projectile_texture_name, 2000, True, 5)
         self.imageSize = Point(24,4)
-        #picked utterly random numbers, we can sort that later. 
-    
+        #picked utterly random numbers, we can sort that later.
+
     def FireAtTarget(self, angle, weapon_force, bl, parentTroop):
         projectile = super(Lazer, self).FireAtTarget(angle,weapon_force,bl,parentTroop)
         projectile.dontApplyGravity()
