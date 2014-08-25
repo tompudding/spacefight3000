b869eecb8494e4acec7f18b10bd383d74448b9db
Paul Fysh <paulfysh@hotmail.co.uk>
2014-08-25 15:51:55 +0100

    grenades last for a timeframe from creation

--- gobjects/grenade.py
+++ gobjects/grenade.py
@@ -8,3 +8,8 @@ class Grenade(weapon.Weapon):
         self.projectile_texture_name = "grenade.png" 
         super(Grenade, self).__init__(50, self.projectile_texture_name, 4000, True, 3)
         self.imageSize = Point(25, 32)
+        
+    def FireAtTarget(self, angle, weapon_force, bl, parentTroop):
+        projectile = super(Grenade, self).FireAtTarget(angle,weapon_force,bl,parentTroop)
+        projectile.destroyAfterTimeLimit()
+        return projectile
